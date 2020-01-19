e8bd0b203cceb5e39a8b09ec838da0bc9a434372
WonJik Yang <yangw783@newschool.edu>
2020-01-19 19:27:51 +0100

    Updates

---
+++ precache-manifest.34ffe7dfa051704e006da4ac70a53d8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa539dff36fe691f2d5c3b7ee18a3fde",
+    "url": "/gastradeflow/index.html"
+  },
+  {
+    "revision": "902fd471cd7d62e9951c",
+    "url": "/gastradeflow/static/css/main.84f78edf.chunk.css"
+  },
+  {
+    "revision": "518ab03ec370e987f33b",
+    "url": "/gastradeflow/static/js/2.c0bb4b27.chunk.js"
+  },
+  {
+    "revision": "ec0d0023e82f5c64f988105105b4e9bd",
+    "url": "/gastradeflow/static/js/2.c0bb4b27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "902fd471cd7d62e9951c",
+    "url": "/gastradeflow/static/js/main.1676857c.chunk.js"
+  },
+  {
+    "revision": "b95aaed126c09ccc32be",
+    "url": "/gastradeflow/static/js/runtime-main.e644e28e.js"
+  }
+]);
